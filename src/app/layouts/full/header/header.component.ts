053ecf9f5d49dd27ab1144a7adc5de3a366a2111
leanhhuy <32382671+leanhhuy@users.noreply.github.com>
2026-01-13 21:28:57 +0100

    Code copied from /wrappixel/material-pro-angular-lite (Angular version 21)

---
+++ src/app/layouts/full/header/header.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import { MaterialModule } from 'src/app/material.module';
+import { RouterModule } from '@angular/router';
+
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { BrandingComponent } from '../sidebar/branding.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    RouterModule,
+    NgScrollbarModule,
+    TablerIconsModule,
+    MaterialModule,
+    BrandingComponent
+],
+  templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.None,
+})
+export class HeaderComponent {
+  @Input() showToggle = true;
+  @Input() toggleChecked = false;
+  @Output() toggleMobileNav = new EventEmitter<void>();
+}
